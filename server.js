fd3ec880b1dad46879a7b967c279ac0a3c81425d
BradleyK <bradleyjkennett@gmail.com>
2022-06-26 21:43:51 -0400

    post request add

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 3001
 
 const app = express();
 
+const fs = require('fs');
+
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json()); 
+
 
 
 // app.get('/api/animals', (req, res) => {
@@ -59,6 +67,37 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimals(body, animalsArray) {
+   //  console.log(body);
+   // our functions main code will go here!
+   const animal = body;
+   animalsArray.push(animal);
+   fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+   );
+   //return finished code to post route for response
+   return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  } 
+
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+
+}
+
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -78,6 +117,20 @@ app.get('/api/animals/:id', (req, res) => {
   }
 }); 
 
+app.post('/api/animals',(req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+ 
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimals(req.body, animals);
+    res.json(animal);
+  }
+});
+
 // app.listen(3001, () => {
 //     console.log(`API server now on port 3001!`);
 // });
@@ -85,3 +138,5 @@ app.listen(PORT, () => {
   // console.log(`API server now on port 3001`)
   console.log(`api server on port ${PORT}!`)
 });
+
+// add validationto our data
